9a00b483b62f98c492f97e6d364c856139e932ee
arthurvelasco406 <148298101+arthurvelasco406@users.noreply.github.com>
2025-03-03 15:48:05 -0300

    Add files via upload

---
+++ test/testeMain.java
@@ -0,0 +1,22 @@
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class testeMain {
+    public static void main(String[] args) throws IOException {
+        
+            }
+    
+}
